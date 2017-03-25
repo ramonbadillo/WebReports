5ff38d1f7f9f4186492cffb8af3231a413479324
fcabada <fatycabada@gmail.com>
2017-03-25 14:22:51 -0600

    add changes to Login Form

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -60,7 +60,10 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            $session = Yii::$app->session;
+            $session->set('usertype', $this->getUser()->tipo);
+            $session->set('userid', $this->getUser()->id);
+           return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
         }
         return false;
     }
